9728359d9139f0da8e990c5b0f9a3bb043178ab4
stannardtyler <tstannard64@gmail.com>
2023-10-26 11:22:06 -0700

    Added p5js project files to repo

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let data;
+let url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQADMbvtgD_CBG5kIv64DtRNuqYIooQd5F0QiJWi46L2A96U_xoHd-oe-bxMO6rqCMcMsAKKA9H2PF5/pub?gid=0&single=true&output=csv";
+
+
+function preload() {
+  data = loadTable(url, 'csv', 'header');
+}
+
+function setup() {
+  let canvas = createCanvas(400, 400);
+  canvas.parent('data');
+  //noLoop();
+
+}
+
+function draw() {
+  background(0);
+
+  if (data) {
+    let numRows = data.getRowCount();
+
+    let carbs = data.getColumn('Carbs');
+    let names = data.getColumn('Food');
+
+    rect(50, 350, carbs[0], 10);
+
+    for (let i = 0; i < numRows; i++) {
+      let x = 50;
+      let y = 100 + (i * 50);
+      let w = carbs[i]*5;
+      let h = 10;
+
+      rect(x, y, w, h);
+
+      fill(255);
+      textSize(14);
+      text(names[i], x, y - 5);
+    }
+  }
+}
